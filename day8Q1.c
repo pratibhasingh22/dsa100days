f936429a28eaa5a2f9d3cc661cf66377c20aa06b
pratibhasingh22 <pratibharanawat22062007@gmail.com>
2026-02-10 09:32:08 +0530

    eight

---
+++ day8Q1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int power(int a,int b){
+    if (b==0)
+      return 1;
+    return a*power(a,b-1);  
+
+}
+int main(){
+    int a,b;
+    printf("enter the values of a and b");
+    scanf("%d",&b);
+    printf("%d",power(a,b));
+    return 0;
+
+
+}
