4455da17ed494cc92130c6716f8f95c2491b7395
pratibhasingh22 <pratibharanawat22062007@gmail.com>
2026-02-20 00:30:20 +0530

    seventeen

---
+++ day18Q1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void reverse(int arr[],int start,int end){
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+
+}
+int main(){
+    int n;
+    printf("enter the no");
+    scanf("%d",&n);
+
+    int arr[n];
+    printf("enter the elements");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int k;
+    scanf("%d",&k);
+
+    k=k%n;
+
+    reverse(arr,0,n-1);
+    reverse(arr,0,k-1);
+    reverse(arr,k,n-1);
+
+    for(int i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
